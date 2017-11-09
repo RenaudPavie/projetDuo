05c959d10b5fb53558fa791dbdc245e08f31dc8a
Unknown <renaud.hpavie@gmail.com>
2017-11-09 14:54:23 +0100

    Ajout de la requête ajax pour récuperer les utilisateurs

--- js/function.js
+++ js/function.js
@@ -3,4 +3,11 @@ $.ajax({
 	method : 'get',
 }).done(function(newUser) {
 	console.log(newUser);
-})
+});
+
+$.ajax({
+	url : 'http://messenger.api.niamor.com/getUsers',
+	method : 'get',
+}).done(function(getUser){
+	console.log(getUser)
+});
